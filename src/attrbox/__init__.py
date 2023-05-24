fcb9c9963fc93b7a2141bd5340830651f7494b55
The Metaist <metaist@metaist.com>
2023-05-23 23:51:54 -0400

    refactor: types, examples

--- src/attrbox/__init__.py
+++ src/attrbox/__init__.py
@@ -3,6 +3,14 @@
 .. include:: ../../README.md
    :start-line: 4
 """
+
+# pkg
+from .attrdict import AttrDict, __pdoc__ as doc1
+from .attrlist import AttrList, __pdoc__ as doc2
+from .fn import Fn
+
+from .jsend import JSend
+
 __version__ = "0.1.1"
 __all__ = (
     "__version__",
@@ -12,9 +20,7 @@ __all__ = (
     "JSend",
 )
 
-# pkg
-from .attrdict import AttrDict
-from .attrlist import AttrList
-from .fn import Fn
-
-from .jsend import JSend
+# Update pdoc at all levels.
+__pdoc__ = {}
+for doc in [doc1, doc2]:
+    __pdoc__.update(doc)
--- test/test_attrdict.py
+++ test/test_attrdict.py
@@ -4,8 +4,12 @@
 
 # native
 from pathlib import Path
+from typing import Any
+from typing import Dict
 import json
-import toml
+
+# lib
+import toml  # TODO 2026-10-04 [3.10 EOL]: switch to tomllib
 
 # pkg
 from attrbox import AttrDict
@@ -13,21 +17,79 @@ from attrbox import AttrDict
 HERE = Path(__file__).parent
 
 
-def test_lshift_json():
+def test_lshift_json() -> None:
     """Expect to load JSON file."""
     # See: https://cloud.google.com/appengine/docs/admin-api/creating-config-files
     path = HERE / "example-appengine.json"
     config = AttrDict() << json.load(path.open(encoding="utf-8"))
-    value = config.deployment.files["example-resource-file1"].sourceUrl
-    expect = "https://storage.googleapis.com/[MY_BUCKET_ID]/example-application/example-resource-file1"
-    assert value == expect, "expect to get valid value"
+
+    have = config.get(["deployment", "files", "example-resource-file1", "sourceUrl"])
+    want = "https://storage.googleapis.com/[MY_BUCKET_ID]/example-application/example-resource-file1"
+    assert want == have, "expect to get valid value"
 
 
-def test_lshift_toml():
+def test_lshift_toml() -> None:
     """Expect to load TOML document."""
     # See: https://github.com/toml-lang/toml
     path = HERE / "example-toml.toml"
     config = AttrDict() << toml.load(path.open(encoding="utf-8"))
-    value = config.servers.alpha.ip
-    expect = "10.0.0.1"
-    assert value == expect, "expect to get valid value"
+
+    want = "10.0.0.1"
+    have = config.get("servers.alpha.ip".split("."))
+    assert want == have, f"expect to get {want}"
+
+
+def test_merge_empty() -> None:
+    """Expect no change."""
+    a = AttrDict(a=1, b=2)
+    b: Dict[Any, Any] = {}
+
+    want = dict(a=1, b=2)
+    have = a << b
+    assert want == have, "expect no change"
+
+
+def test_update() -> None:
+    """Expect a basic merges to work."""
+    a = AttrDict(a=1, b=2)
+    b = dict(a=3)
+
+    want = dict(a=3, b=2)
+    have = a << b
+    assert want == have, "expect overwrite one value"
+
+    a = AttrDict(a=1, b=2)
+    b = dict(a=3, b=4)
+    want = dict(a=3, b=4)
+    have = a << b
+    assert want == have, "expect overwrite both"
+
+
+def test_extend() -> None:
+    """Expect to add new value."""
+    a = AttrDict(a=1, b=2)
+    b = dict(c=3)
+
+    want = dict(a=1, b=2, c=3)
+    have = a << b
+    assert want == have, "expect new value"
+
+
+def test_nested() -> None:
+    """Expect basic nesting to merge."""
+    a = AttrDict(a=1, b=dict(c=3, d=4))
+    b = dict(b=dict(c=5))
+
+    want = dict(a=1, b=dict(c=5, d=4))
+    have = a << b
+    assert want == have, "expect 1-nested merge"
+
+
+def test_double_nested() -> None:
+    """Expect double nesting to merge."""
+    a = AttrDict(a=1, b=dict(c=3, d=dict(e=4, f=5)))
+    b = dict(b=dict(d=dict(f=6)))
+
+    want = dict(a=1, b=dict(c=3, d=dict(e=4, f=6)))
+    have = a << b
+    assert want == have, "expect 2-nested merge"
